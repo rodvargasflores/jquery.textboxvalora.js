f2cb8719e7f3710179ecf0709f2cc16a5f08bde5
root <root@dev0.dev.nubedas.com>
2016-01-12 18:32:38 -0300

    Se sube version alpha normal y minimal

--- README.md
+++ README.md
@@ -1,1 +1,238 @@
-# jquery.textboxvalora.js
+# Plugin textboxValora
+<p>Plugin que sirve para precargar la librería <a href="https://textbox.io/" target="_blank" rel="nofollow">textboxio</a> de ephox de acuerdo a estándar de ecosistema VALORA.</p>
+<p>El plugin registra una variable global de JavaScript para acceder a sus métodos. Esta variable global, denominada <strong>textboxValora</strong>, se encuentra disponible una vez que se haya cargado el plugin.</p>
+<p><strong>No es necesario tener cargada la librería de textboxio al momento de cargar el documento</strong>. El plugin identificará si existe la instancia. En caso no existir, intentará cargarla de manera dinámica. Para esto, la librería debe estar en la ruta predeterminada de plugins de ecosistema VALORA: <code><strong>/assets/vendor/plugins/textboxio/textboxio.js</strong></code></p>
+<p>Requiere de JQuery1.10+ y librería <a href="https://textbox.io/" target="_blank" rel="nofollow">textboxio</a>.</p>
+
+## Readme en construcción
+
+## Métodos
+<table>
+  <tbody>
+    <tr>
+      <td colspan="1">
+        <code><span><a href="#replace">replace</a><br></span></code>
+      </td>
+      <td colspan="1">
+        Reemplaza un elemento del DOM con una instancia del Editor.
+      </td>
+    </tr>
+    <tr>
+      <td colspan="1">
+        <code><span><a href="#triggerSave">triggerSave</a><br></span></code>
+      </td>
+      <td colspan="1">
+        Recorre los elementos reemplazados por el editor y guarda su contenido en el elemento original.
+      </td>
+    </tr>
+    <tr>
+      <td colspan="1">
+        <code><span><a href="#get">get</a><br></span></code>
+      </td>
+      <td colspan="1">
+        Obtiene instancia (objeto) del editor.
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+## Replace
+Crea instancias del editor reemplazando elementos como <code>&lt;textarea&gt;</code> o <code>&lt;div&gt;</code>.
+Cuando se reemplaza un elemento <code>&lt;textarea&gt;</code> al interior de un <code>&lt;form&gt;</code>, este actualizará el textarea original cuando el form sea enviado.
+
+####Ejemplos<br>
+<strong>textboxValora.replace(selector, [configuración])</strong>
+```javascript
+	<div id="replaceMe">Contenido</div>
+	...
+	// Crea un editor buscando el elemento con id 'replaceMe'
+	var simpleEditor = textboxValora.replace( '#replaceMe' );
+```
+
+<strong>textboxValora.replace(elemento, [configuración])</strong>
+```javascript
+	<div id="replaceMe">Contenido</div>
+	...
+	// Crea un editor reemplazando un elemento específico
+	var div = document.getElementById('replaceMe');
+	var simpleEditor = textboxValora.replace( div );
+ ```
+#### Parámetros
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <p>
+          <span style="color: rgb(0,0,0);"><code>selector</code></span>
+        </p>
+        <p>
+          <span style="color: rgb(0,0,0);">o</span>
+        </p>
+        <p>
+          <code><span style="color: rgb(0,0,0);">elemento</span></code>
+        </p>
+      </td>
+      <td colspan="1">
+        <p>String</p>
+        <p>o</p>
+        <p>Elemento</p>
+      </td>
+      <td>
+        <p>
+          <a href="http://www.w3.org/TR/css3-selectors/" target="_blank" rel="nofollow">Selector CSS3</a> <span>para reemplazar el elemento <code>&lt;div&gt;</code> o <code>&lt;textarea&gt;</code>.
+          </span>
+        </p>
+        <p>
+          <span>o</span>
+        </p>
+        <p>
+          <span>Elemento</span> <code>&lt;div&gt;</code> <span> o </span> <code>&lt;textarea&gt;</code>
+          <span> que desea reemplazar.</span>
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="1">
+          <code><span><a href="#configuración">configuración</a><br></span></code>
+      </td>
+      <td colspan="1">
+        Objeto (opcional)
+      </td>
+      <td>
+          Grupo opcional que especifica opciones/configuraciones al inicializar las instancias que está invocando.
+      </td>
+    </tr>
+  </tbody>
+</table>
+#### Return
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code><a href="http://docs.ephox.com/display/tbio/editor" target="_blank" rel="nofollow">textboxio.editor</a></code>
+        </span>
+      </td>
+      <td colspan="1">Objeto</td>
+      <td>Instancia única del editor.</td>
+    </tr>
+  </tbody>
+</table>
+
+## Configuración
+####Ejemplos<br>
+<strong>Valores por defecto</strong>
+```javascript
+	var options = {
+		debug_mode : false,
+		toolbar : 'normal',
+		img_upload_url : '/default/subir-complemento/',
+		resize_uploaded_img : {
+			enabled: false,
+			maxWidth: 600,
+			maxHeight: 600
+		}
+	}
+```
+#### Propiedades del objeto
+Todas las propiedades son opcionales. Definir alguna sobreescribirá la opción predeterminada del plugin.
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code>debug_mode</code>
+        </span>
+      </td>
+      <td colspan="1">Boolean</td>
+      <td>
+        Permite revisar en consola el paso a paso de ingreso a funciones y entrega información más detallada en caso de lanzar un error.
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code>toolbar</code>
+        </span>
+      </td>
+      <td colspan="1">String</td>
+      <td>
+        <p>
+          <strong>"tiny":</strong> (String) Crea el editor con una barra de herramientas básica.
+        </p>
+        <p>
+          <strong>"normal":</strong> (String) Crea el editor con una barra de herramientas normal (incluye upload de imágenes).
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code>img_upload_url</code>
+        </span>
+      </td>
+      <td colspan="1">String</td>
+      <td>
+        <p>
+          URL donde se enviará el archivo para guardarlo en el servidor. Es un ajax de tipo JSON y envía dos parámetros, uno de tipo <code><strong>File</strong></code> y el segundo es un <code><strong>String</strong></code> con el nombre del archivo enviado. El nombre de los parámetros son <code><strong>file</strong></code> y <code><strong>name</strong></code> respectivamente.
+        </p>
+        <p>
+        	<strong>El retorno debe ser un JSON con los siguientes parámetros:</strong>
+        </p>
+        <p>
+          <strong>"result":</strong> (Boolean) Permite identificar si la carga del archivo fue exitosa
+        </p>
+        <p>
+          <strong>"debug":</strong> (String) Mensaje personaliza del error en la subida.
+        </p>
+        <p>
+          <strong>"ruta":</strong> (String) URL de acceso al archivo.
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code>img_upload_response</code>
+        </span>
+      </td>
+      <td colspan="1">Objeto</td>
+      <td>
+        <p>
+          Callbacks de respuesta para la cargar de archivos en el editor.
+        </p>
+        <p>
+          <strong>"img_upload_response.success(json, textStatus, jqXHR)":</strong> Callback que permite realizar alguna acción una vez que la imagen se subió al servidor y fue cargada exitosamente en el editor.
+        </p>
+        <p>
+          <strong>"img_upload_response.fail(jqXHR, textStatus, errorThrown)":</strong> Callback que permite realizar alguna acción en caso de que la subida del archivo haya fallado.
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <span style="color: rgb(0,0,0);">
+          <code>resize_uploaded_img</code>
+        </span>
+      </td>
+      <td colspan="1">Objeto</td>
+      <td>
+        <p>
+          Permite habilitar la redimensión por canvas de la imagen cargada en el servidor.
+        </p>
+        <p>
+          <strong>"enabled":</strong> (Boolean) Habilita o deshabilita la opción de redimensionar.
+        </p>
+        <p>
+          <strong>"maxWidth":</strong> (Numeric) Ancho máximo que se permite antes de reescalar por canvas la redimensión.
+        </p>
+        <p>
+          <strong>"maxHeight":</strong> (Numeric) Alto máximo que se permite antes de reescalar por canvas la redimensión.
+        </p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+## Problemas conocidos (Known Issues)
+- Al estar el editor en modo de pantalla completa y ejecutar el método <strong>textboxValora.triggerSave(selector)</strong>, el plugin arroja error de nodeName.
---
+++ jquery.textboxvalora.js
@@ -0,0 +1,491 @@
+/*
+TextBox v0.1.1
+Plugin que sirve para precargar la librería textboxio de ephox de acuerdo a estándar de ecosistema VALORA
+Autor: Real Ace One
+*/
+if (!window.jQuery === 'undefined') { throw new Error('Cargue primero la librería JQuery') };
+
+(function($){
+	/** Variables de texto **/
+	var txt_library_name 				= 'Textboxio';
+	var txt_fail_ajax_load_js 			= 'No se puede cargar la librería '+txt_library_name+' %s';
+	var txt_fail_img_upload 			= 'Ha ocurrido un problema con la subida del archivo. Por favor intente nuevamente. %s'
+	var txt_fail_load_editors_instance 	= 'No se puede cargar la instancia del editor.';
+	var txt_miss_param 					= 'Falta el parámetro "%s"';
+	var txt_must_be_string 				= '"%s" debe ser un string.';
+	var txt_must_be_object 				= '"%s" debe ser un objeto.';
+	var txt_no_element_found 			= 'No se encontró ningún elemento para reemplazar.';
+	var txt_create_editor_instance 		= 'Se crea instancia de editor en "%s"';
+	var txt_save_content_in 	 		= 'Se guarda contenido en elemento "%s"';
+	var txt_library_not_found 			= 'No se encuentra la librería '+txt_library_name+'. Se intentará cargar dinámicamente.';
+	var txt_success_library_load		= 'Se cargó exitosamente la librería '+txt_library_name+'.';
+	var txt_toolbar_setup 				= 'Se cargó la barra de tareas en modo "%s"';
+	var txt_success_file_upload			= 'Se cargó el archivo en el servidor con ruta de acceso "%s"';
+	var txt_element_save_not_allowed 	= 'El elemento en que se intenta guardar el contenido del editor no está permitido.';
+	var txt_element_not_allowed 		= 'Sólo se permiten elementos de tipo DIV o TEXTAREA.';
+
+	/** Rutas de librerías **/
+	var textboxio_url 					= '/assets/vendor/plugins/textboxio/textboxio.js';
+	var textboxio_basePath 				= '/assets/vendor/plugins/textboxio/';
+
+	/** Variables globales **/
+	// Permite guardar la instancia del editor al crearlo
+	// var editors 						= [];
+ 	// Permite saber si se lanzó el ajax para cargar el JS de TextBoxIO
+	var loadingScript 					= false;
+	// Se vuelve objeto en caso de que el ajax que carga el JS de TextBoxIO retorna algo
+	var scriptLoaded;
+	var defaults,settings,
+	w = window,g,o,s,v;
+
+	/** FUNCIONES PRIVADAS **/
+	var debug = function (d) {
+		console.log(d);
+	}
+
+	var getDefaults = function() {
+		d = {
+			debug_mode : false,
+			toolbar : 'normal',
+			img_upload_url : '/default/subir-complemento/',
+			img_upload_response : {
+				success: function(json, textStatus, jqXHR){},
+				fail: function(jqXHR, textStatus, errorThrown){}
+			},
+			resize_uploaded_img : {
+				enabled : false,
+				maxWidth : 600,
+				maxHeight : 600
+			},
+			config : {
+	            codeview: {
+	                enabled : false,
+	                showButton: false
+	            },
+	            autosubmit : true,
+	            basePath : textboxio_basePath,
+	            images: {
+	                allowLocal: true,
+	                upload : {
+	                    handler : function (data, success, failure) {
+	                        uploadEditorHandler(data.blob(), data.filename(), function(jqxhr){
+	                            // jqxhr.then ??
+	                            jqxhr.done(function(json, textStatus, jqXHR){
+	                                settings.img_upload_response.success(json, textStatus, jqXHR);
+
+	                                if (json.result)
+	                                {
+										if (settings.debug_mode) debug(txt_success_file_upload.replace('%s',json.ruta));
+	                                    success(json.ruta);
+	                                }
+	                                else
+	                                {
+							        	if (settings.debug_mode) debug(jqXHR);
+										throw new Error(txt_fail_img_upload.replace('%s',''));
+									}
+						        }).fail(function(jqXHR, textStatus, errorThrown) {
+	                                settings.img_upload_response.fail(jqXHR, textStatus, errorThrown);
+
+						        	if (settings.debug_mode) debug(jqXHR);
+									throw new Error(txt_fail_img_upload.replace('%s','('+errorThrown+')'));
+								});
+	                        });                         
+	                    }
+	                }
+	            }
+			}
+		}
+
+		// Corregimos el problema del basePath al cargar primero la librería textboxIO
+		if (w.textboxio) delete d.config['basePath'];
+
+		return d;
+	}
+
+	var setEditorUiToolbar = function() {
+		switch(settings.toolbar)
+		{
+			case 'tiny':
+				settings.config['ui'] = {
+	                toolbar : {
+	                    items : [
+	                        {
+	                            label: 'Undo and Redo group',
+	                            items: [ 'undo', 'redo' ]
+	                        },
+	                        {
+	                            label: 'Emphasis group',
+	                            items: [ 'bold', 'italic', 'underline' ]
+	                        },
+	                        {
+	                            label: 'Format group',
+	                            items: [ 'font-menu', 'removeformat' ]
+	                        }
+	                    ]
+	                }
+	            }
+			break;
+
+			case 'normal':
+	            settings.config['ui'] = {
+	                toolbar : {
+	                    items : [
+	                        {
+	                            label: 'Undo and Redo group',
+	                            items: [ 'undo', 'redo' ]
+	                        },
+	                        {
+	                            label: 'Insert group',
+	                            items: [
+	                                { 
+	                                    id    : 'insert',
+	                                    label  : 'Insertar',
+	                                    items : [ 'fileupload', 'table', 'specialchar', 'hr' ]
+	                                }
+	                            ]
+	                        },
+	                        {
+	                            label: 'Styles group',
+	                            items: [ 'styles' ]
+	                        },
+	                        {
+	                            label: 'Emphasis group',
+	                            items: [ 'bold', 'italic', 'underline' ]
+	                        },
+	                        {
+	                            label: 'Align group',
+	                            items: [ 'alignment' ]
+	                        },
+	                        {
+	                            label: 'Listindent group',
+	                            items: [ 'ul', 'ol', 'indent', 'outdent', 'blockquote' ]
+	                        },
+	                        {
+	                            label: 'Format group',
+	                            items: [ 'font-menu', 'removeformat' ]
+	                        },
+	                        {
+	                            label: 'Tools group',
+	                            items: [ 'find', 'fullscreen', 'usersettings' ]
+	                        }
+	                    ]
+	                }
+	            }
+			break;
+		}
+	}
+
+	var buildEditor = function(e) {
+		$.each(e, function(i,v) {
+			if (/TEXTAREA|DIV/i.test(v.nodeName))
+			{
+				try
+				{
+					textboxio.replace(v,settings.config);
+				}
+				catch(e)
+				{
+					if (w.textboxio && !settings.config['basePath'])
+						settings.config['basePath'] = textboxio_basePath;
+					textboxio.replace(v,settings.config);
+				}
+
+				if (settings.debug_mode)
+				{
+					debug(txt_create_editor_instance.replace('%s',v.nodeName));
+					debug(v);
+				}
+			}
+			else
+				throw new Error(txt_element_not_allowed);
+		});
+	}
+
+	var uploadEditorHandler = function (dataBlob, dataFilename, callback) {
+		var resizedImage, jqxhr;
+		var formData  = new FormData();
+
+		// Cargamos la imagen
+		var reader = new FileReader();
+		reader.onload = function (readerEvent) {
+			var image = new Image();
+			image.onload = function (imageEvent) {
+				var canvas 	= document.createElement('canvas');
+
+				resizeCanvasImage(image, canvas, settings.resize_uploaded_img['maxWidth'], settings.resize_uploaded_img['maxHeight']);
+				// var dataUrl         = canvas.toDataURL('image/jpg');
+				// var dataUrl         = canvas.toDataURL('image/png');
+
+				dataUrl 		= canvas.toDataURL();
+				resizedImage 	= dataURLToBlob(dataUrl);
+
+				formData.append('file', resizedImage);
+				formData.append('name', dataFilename);
+
+				jqxhr = $.ajax({
+					url: settings.img_upload_url,
+					type: 'POST',
+					dataType: 'json',
+					async: true,
+					cache: false,
+					data: formData,                            
+					contentType: false,
+					processData: false
+				});
+				
+				callback(jqxhr);
+			}
+			image.src = readerEvent.target.result;
+		}
+		reader.readAsDataURL(dataBlob);
+	}
+
+    var resizeCanvasImage = function(img, canvas, maxWidth, maxHeight) {
+        var imgWidth 	= img.width, 
+            imgHeight 	= img.height;
+
+        var resizeImg 	= (settings.resize_uploaded_img['enabled'] && imgWidth > maxWidth) ? true : false;
+
+        var ratio 		= 1, ratio1 = 1, ratio2 = 1;
+        ratio1 			= maxWidth / imgWidth;
+        ratio2 			= maxHeight / imgHeight;
+
+        // Usamos el ratio más pequeño posible para que la imagen
+        // encaje de mejor manera dentro del maxWidth x maxHeight.
+        if (ratio1 < ratio2) {
+            ratio = ratio1;
+        }
+        else {
+            ratio = ratio2;
+        }
+
+        var canvasContext 	= canvas.getContext("2d");
+        var canvasCopy 		= document.createElement("canvas");
+        var copyContext 	= canvasCopy.getContext("2d");
+        var canvasCopy2 	= document.createElement("canvas");
+        var copyContext2 	= canvasCopy2.getContext("2d");
+
+        canvasCopy.width 	= imgWidth;
+        canvasCopy.height 	= imgHeight;
+        copyContext.drawImage(img, 0, 0);
+
+        // init
+        canvasCopy2.width 	= imgWidth;
+        canvasCopy2.height 	= imgHeight;        
+        copyContext2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
+
+
+        // Si aumentamos los rounds, obtenemos una imagen con más efecto antialiasing
+        var rounds 			= 2;
+        var roundRatio 		= ratio * rounds;
+        for (var i = 1; i <= rounds; i++) {
+            // debug("Paso: "+i);
+
+            // temporal
+            if (resizeImg)
+            {
+                canvasCopy.width 	= imgWidth * roundRatio / i;
+                canvasCopy.height 	= imgHeight * roundRatio / i;
+            }
+            else
+            {
+                canvasCopy.width 	= imgWidth;
+                canvasCopy.height 	= imgHeight;
+            }
+
+            copyContext.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvasCopy.width, canvasCopy.height);
+
+            // copiamos de vuelta
+            if (resizeImg)
+            {
+                canvasCopy2.width 	= imgWidth * roundRatio / i;
+                canvasCopy2.height 	= imgHeight * roundRatio / i;
+            }
+            else
+            {
+                canvasCopy2.width 	= imgWidth;
+                canvasCopy2.height 	= imgHeight;
+            }
+            copyContext2.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvasCopy2.width, canvasCopy2.height);
+
+        }
+
+
+        // copiamos de vuelta al canvas
+        if (resizeImg)
+        {
+            canvas.width 	= imgWidth * roundRatio / rounds;
+            canvas.height 	= imgHeight * roundRatio / rounds;
+        }
+        else
+        {
+            canvas.width 	= imgWidth;
+            canvas.height 	= imgHeight;
+        }
+        canvasContext.drawImage(canvasCopy2, 0, 0, canvasCopy2.width, canvasCopy2.height, 0, 0, canvas.width, canvas.height);
+
+        return;
+    }
+
+    var dataURLToBlob = function(dataURL) {
+        var BASE64_MARKER = ';base64,';
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            var parts = dataURL.split(',');
+            var contentType = parts[0].split(':')[1];
+            var raw = parts[1];
+
+            return new Blob([raw], {type: contentType});
+        }
+
+        var parts = dataURL.split(BASE64_MARKER);
+        var contentType = parts[0].split(':')[1];
+        var raw = window.atob(parts[1]);
+        var rawLength = raw.length;
+
+        var uInt8Array = new Uint8Array(rawLength);
+
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([uInt8Array], {type: contentType});
+    }
+
+	var ephox_plugin_loader = function() {
+		// Si no hemos iniciado aún la carga el JS
+		if (!loadingScript)
+		{
+			scriptLoaded = $.ajax({
+	            dataType: "script",
+	            cache: false,
+	            async: true,
+	            url: textboxio_url,
+	            beforeSend: function(){
+	            	loadingScript = true;
+	            }
+	    	})
+	        .fail(function(jqXHR, textStatus, errorThrown) {
+	        	if (settings.debug_mode) debug(jqXHR);
+				throw new Error(txt_fail_ajax_load_js.replace('%s','('+errorThrown+')'));
+			});
+		}
+	}
+
+	// Corrección de errores
+	var fixIssues = function() {
+	}
+	/** FIN DE FUNCIONES PRIVADAS **/
+
+	/** COMENZAMOS FUNCIONES DEL PLUGIN **/
+	var v = {
+		get: function(selector) {
+			if (arguments.length == 0)
+				throw new Error(txt_miss_param.replace('%s','selector'));
+
+			if (typeof selector !== 'string')
+				throw new Error(txt_must_be_string.replace('%s','Selector'));
+
+			return textboxio.get(selector);
+		},
+		triggerSave: function(selector) {
+			if (arguments.length == 0)
+				throw new Error(txt_miss_param.replace('%s','selector'));
+
+			if (typeof selector !== 'string')
+				throw new Error(txt_must_be_string.replace('%s','Selector'));
+
+			editors = v.get(selector);
+			if (editors.length > 0)
+			{
+				$.each(editors,function(i,e){
+					// console.log($(document).find(e.element()));
+					r = $(document).find(e.element()).parent();
+					if (r.length > 0)
+					{
+						t = r.next();
+						c = e.content.get();
+						if (/TEXTAREA/i.test(t[0].nodeName))
+						{
+							t[0].innerHTML = c;
+						
+							if (settings.debug_mode)
+							{
+								debug(txt_save_content_in.replace('%s',t[0].nodeName));
+								debug(t[0]);
+							}
+						}
+						else
+						{
+							if (settings.debug_mode)
+							{
+								// debug(txt_element_save_not_allowed);
+								// debug(t[0]);
+							}
+						}
+
+					}
+				});
+			}
+			else
+				throw new Error(txt_fail_load_editors_instance);
+		},
+		replace: function(e,o) {
+			if (arguments.length == 0)
+				throw new Error(txt_miss_param.replace('%s','selector'));
+
+			if (typeof e !== 'string')
+				throw new Error(txt_must_be_string.replace('%s','Selector'));
+
+			if (typeof o !== 'undefined' && typeof o !== 'object')
+			 	throw new Error(txt_must_be_object.replace('%s','Argument 2'));
+
+			e = $(e);
+
+			// Configuración predeterminada
+			defaults = getDefaults();
+			// Hacemos merge de las configuraciones defaults y las dadas por el cliente
+			settings = $.extend(true, {}, defaults, o);
+			// Obtenemos el toolbar del editor
+			setEditorUiToolbar();
+			// Corrige problemas de compatibilidad del plugin
+			// fixIssues();
+
+			if(e.length > 0)
+			{
+				// Preguntamos si no existe la librería de textboxIO, para así intentar cargarla
+				if (!w.textboxio)
+				{
+					if (settings.debug_mode) debug(txt_library_not_found);
+
+					ephox_plugin_loader();
+					
+					// Aplicamos un timer para saber que se carga una sola vez el ajax. Esto es porque el param async
+					// del ajax va true. False está deprecado y genera warning en la consola.
+					var timerScript = w.setInterval(function() {
+						if(scriptLoaded.readyState == 4 && scriptLoaded.status == 200)
+						{
+							if (settings.debug_mode)
+							{
+								debug(txt_success_library_load);
+								debug(txt_toolbar_setup.replace('%s',settings.toolbar));
+							}
+
+							clearInterval(timerScript);
+							buildEditor(e);
+						}
+					}, 1000);
+				}
+				else
+					buildEditor(e);
+			}
+			else
+			{
+				if (settings.debug_mode) debug(txt_no_element_found);
+			}
+		}
+	}
+	/** FIN DE LAS FUNCIONES DEL PLUGIN **/
+
+	// Publicamos la variable global de acceso al plugin
+	w.textboxValora = v;
+}(jQuery));
